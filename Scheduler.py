846de71369ce7db05a4d3c67b75d33fc04b6ca80
Alex <77426531+Haas-A@users.noreply.github.com>
2023-01-27 16:48:27 -0500

    Changed format of csv input file, continued with manipulations of the list of teams, rivals. Ready to write part that actually creates the schedule.

--- Scheduler.py
+++ Scheduler.py
@@ -1,17 +1,32 @@
 import csv
 
-filename = 'FourDiv.csv'
-
+filename = 'NewFormat.csv'
 file = open(filename, 'r')
-
 data = csv.DictReader(file)
 
-numDivs = len(data.fieldnames)
+Teams = []
+Rivals = []
+WeekList = []
 
-Divs = {}
 for row in data:
-    for i in range(1, numDivs):
-        print(i)
-        print(row[str(i)])
+    Teams.append(row['Teams'])
+    Rivals.append(row['Rival'])
+    WeekList.append(row['Weeks'])
 
-print(data.fieldnames)
+WeeksLeft = WeekList[0]
+#print(str(Rivals))
+#print("Teams: " + str(Teams))
+#print("Weeks Remaining: " + WeeksLeft)
+
+#Now all of the teams and their rivals are stored
+League = []
+
+for i in range(0,len(Teams)):
+    currentTeam = Teams[i]
+    val = Teams.remove(currentTeam)
+    League.append((currentTeam, Rivals[i], Teams[0:len(Teams)]))
+    Teams.insert(i, currentTeam)
+
+print(League[1])
+
+#Now there is a list with each team, its rival, and each of the teams it must have on their schedule.
